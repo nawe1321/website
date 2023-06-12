65d70ac76b4e96d487576d7aa3fc6983826eabfe
Nathan <nathan.wendlowsky@flatironschool.com>
2023-06-12 12:05:59 -0700

    Added button to CSS/HTML/JS

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 document.querySelector("body").appendChild(h2);
+
+const myButton = document.getElementById("myButton");
+
+myButton.addEventListener("mouseover", function() {
+  myButton.style.backgroundColor = "#ff9999";
+});
+
+myButton.addEventListener("mouseout", function() {
+  myButton.style.backgroundColor = "#f2f2f2";
+});
